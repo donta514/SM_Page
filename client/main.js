15f003e1e8535ad6c65840c3960ead16f7c8f8ac
donta514 <58492506+donta514@users.noreply.github.com>
2022-02-25 14:58:59 -0500

    flip card updates

--- client/main.js
+++ client/main.js
@@ -1,71 +1,89 @@
-document.querySelectorAll(".card").forEach((card) => {
-  // console.log(card)
-  card.addEventListener("click", (e) => {
-    console.log(card.children);
-    const front = card.children[0];
-    const back = card.children[1];
-    console.log(gsap);
-    console.log("hey");
-    card.classList.toggle("active");
-
-    // const duration = 1;
-    // //create gsap timeline
-    // let tlfwd = gsap.timeline();
-    // tlfwd.to(front, { opacity: 0, duration });
-    // tlfwd.to(back, { opacity: 1, duration }, `-=${duration}`);
-
-    // let tlback = gsap.timeline();
-    // tlback.to(front, { opacity: 0, duration });
-    // tlback.to(back, { opacity: 1, duration }, `-=${duration}`);
-
-    // if (card.classList.contains("active")) {
-    //   console.log("deactivate");
-    //   tlfwd.play();
-    // } else {
-    //   console.log("activate");
-    //   tlback.play();
-    // }
-    const duration = 0.5;
-    if (card.classList.contains("active")) {
-      console.log("deactivate");
-      gsap.to(front, {
-        duration,
-        opacity: 0,
-      });
-      gsap.to(back, {
-        duration,
-        opacity: 1,
-      });
-    } else {
-      console.log("activate");
-      gsap.to(back, {
-        duration,
-        opacity: 0,
-      });
-      gsap.to(front, {
-        duration,
-        opacity: 1,
-      });
-    }
+var cards = document.querySelectorAll("div.flip");
+console.log(cards)
+for (let card of cards) {
+  card.addEventListener("click", function () {
+    card.classList.toggle("is-flipped");
   });
-});
+}
+
+// var card = document.querySelector(".cardtw");
+// card.addEventListener("click", function () {
+//   card.classList.toggle("is-flipped");
+// });
+
+// var cardtw = document.querySelector(".card");
+// cardt.addEventListener("click", function () {
+//   card.classList.toggle("is-flipped");
+// });
+
+// document.querySelectorAll(".card").forEach((card) => {
+//   console.log(card)
+//   card.addEventListener("click", (e) => {
+//     console.log(card.children);
+//     const front = card.children[0];
+//     const back = card.children[1];
+//     console.log(gsap);
+//     console.log("hey");
+//     card.classList.toggle("active");
+
+//     const duration = 1;
+//     //create gsap timeline
+//     let tlfwd = gsap.timeline();
+//     tlfwd.to(front, { opacity: 0, duration });
+//     tlfwd.to(back, { opacity: 1, duration }, `-=${duration}`);
+
+//     let tlback = gsap.timeline();
+//     tlback.to(front, { opacity: 0, duration });
+//     tlback.to(back, { opacity: 1, duration }, `-=${duration}`);
+
+//     if (card.classList.contains("active")) {
+//       console.log("deactivate");
+//       tlfwd.play();
+//     } else {
+//       console.log("activate");
+//       tlback.play();
+//     }
+//     const duration = 0.5;
+//     if (card.classList.contains("active")) {
+//       console.log("deactivate");
+//       gsap.to(front, {
+//         duration,
+//         opacity: 0,
+//       });
+//       gsap.to(back, {
+//         duration,
+//         opacity: 1,
+//       });
+//     } else {
+//       console.log("activate");
+//       gsap.to(back, {
+//         duration,
+//         opacity: 0,
+//       });
+//       gsap.to(front, {
+//         duration,
+//         opacity: 1,
+//       });
+//     }
+//   });
+// });
 
 // if file is not pulling consider adding no cors mode back in
 
-const getAndSetStats = async () => {
-  const response = await fetch('/api/socialstats');
-  const socialStats = await response.json();
-  console.log(socialStats);
-  document.getElementById("facebookCount").innerHTML = socialStats.facebook;
-  document.getElementById("twitterCount").innerHTML = socialStats.twitter;
-  document.getElementById("ytCount").innerHTML = socialStats.youtube;
-  document.getElementById("igCount").innerHTML = socialStats.instagram;
-  document.getElementById("linkedinCount").innerHTML = socialStats.linkedin;
-  document.getElementById("tiktokCount").innerHTML = socialStats.tiktok;
-  document.getElementById("parlerCount").innerHTML = socialStats.parler;
-  document.getElementById("rumbleCount").innerHTML = socialStats.rumble;
-  document.getElementById("gettrCount").innerHTML = socialStats.gettr;
-  document.getElementById("clapperCount").innerHTML = socialStats.clapper;
-};
+// const getAndSetStats = async () => {
+//   const response = await fetch('/api/socialstats');
+//   const socialStats = await response.json();
+//   console.log(socialStats);
+//   document.getElementById("facebookCount").innerHTML = socialStats.facebook;
+//   document.getElementById("twitterCount").innerHTML = socialStats.twitter;
+//   document.getElementById("ytCount").innerHTML = socialStats.youtube;
+//   document.getElementById("igCount").innerHTML = socialStats.instagram;
+//   document.getElementById("linkedinCount").innerHTML = socialStats.linkedin;
+//   document.getElementById("tiktokCount").innerHTML = socialStats.tiktok;
+//   document.getElementById("parlerCount").innerHTML = socialStats.parler;
+//   document.getElementById("rumbleCount").innerHTML = socialStats.rumble;
+//   document.getElementById("gettrCount").innerHTML = socialStats.gettr;
+//   document.getElementById("clapperCount").innerHTML = socialStats.clapper;
+// };
 
-getAndSetStats();
+// getAndSetStats();
